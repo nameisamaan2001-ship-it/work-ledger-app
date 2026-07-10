5d176e1c21710de2c9ab12b97b0de20a21116900
nameisamaan2001-ship-it <nameisamaan2001@gmail.com>
2026-07-10 22:15:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/workledger/app/MainActivity.java
+++ app/src/main/java/com/workledger/app/MainActivity.java
@@ -3,6 +3,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebChromeClient;
 
 public class MainActivity extends Activity {
     @Override
@@ -12,6 +13,10 @@ public class MainActivity extends Activity {
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowUniversalAccessFromFileURLs(true);
+        s.setAllowFileAccessFromFileURLs(true);
+        webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
     }
